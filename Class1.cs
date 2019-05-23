f35b39a61b24e4d7b50c4de1450b7937146c217c
SinghNavneet93 <50494486+SinghNavneet93@users.noreply.github.com>
2019-05-23 10:41:48 -0400

    Add files via upload
    First Commit addition file

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AwesomeCalculator;
+using NUnit.Framework;
+
+
+namespace CalcAppTest
+
+{
+    [TestFixture]
+    class CalcTests
+    {
+        [Test]
+        public void GetAddition_Input5and6_Returns11point0()
+        {
+
+            //Arrange
+            double number1 = 5;
+            double number2 = 6;
+
+            double expectedResult = number1 + number2;
+
+            Calc testCalc = new Calc(number1, number2);
+
+            //Act
+            double actualResult = testCalc.GetAddition();
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+    }
+
+}
